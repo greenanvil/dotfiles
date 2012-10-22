2a857c3075bbf37654d0cca942e4e890f8b57cc7
dsmith <dev@greenanvil.com>
2012-10-22 00:02:49 -0700

    Slightly modified for non-OS X systems

---
+++ create_symlinks.sh
@@ -0,0 +1,13 @@
+# This very stupidly creates the necessary symlinks but does not check to see if files exist and certainly doesn't do
+# anything with the contents of any existing files (for now)
+
+# TODO: Handle files if they exist
+
+sudo ln -s $PWD/.aliases ~/.aliases
+sudo ln -s $PWD/.bash_profile ~/.bash_profile
+sudo ln -s $PWD/.bash_prompt ~/.bash_prompt
+sudo ln -s $PWD/.bashrc ~/.bashrc
+sudo ln -s $PWD/.exports ~/.exports
+sudo ln -s $PWD/.functions ~/.functions
+
+
